969fd392cab748b48b9d45c465df88fe9f60d9c3
FLYINGKRIPTO <srivastava.shivanshu4@gmail.com>
2019-01-02 20:56:45 +0530

    full code implemented onMapReady

--- app/src/main/java/com/example/dell/udemyapp79mapsandspeech/MapsActivity.java
+++ app/src/main/java/com/example/dell/udemyapp79mapsandspeech/MapsActivity.java
@@ -1,9 +1,13 @@
 package com.example.dell.udemyapp79mapsandspeech;
 
+import android.content.Intent;
 import android.graphics.Color;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 
+import com.example.dell.udemyapp79mapsandspeech.Model.CountryDataSource;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -13,14 +17,25 @@ import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.io.IOException;
+import java.util.List;
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private String receivedCountry;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
+        Intent mainActivityIntent = this.getIntent();
+        receivedCountry = mainActivityIntent.getStringExtra(CountryDataSource.COUNTRY_KEY);
+        if(receivedCountry == null){
+            receivedCountry = CountryDataSource.DEFAULT_COUNTRY_NAME;
+        }
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
@@ -48,22 +63,51 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        //Camera zooming
-        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(sydney,10.0f);
+         double countryLatitude = CountryDataSource.DEFAULT_COUNTRY_LATITUDE;
+         double countryLongitude = CountryDataSource.DEFAULT_COUNTRY_LONGITUDE;
+
+         CountryDataSource countryDataSource = MainActivity.countryDataSource;
+         String countryMessage = countryDataSource.getInfoOfTheCountry(receivedCountry);
+
+         //Geocoder
+        try {
+            Geocoder geocoder  = new Geocoder(MapsActivity.this);
+            String countryAddress = receivedCountry;
+            List<Address> countryAddresses = geocoder.getFromLocationName(countryAddress,10);
+
+            if(countryAddresses != null){
+                countryLatitude = countryAddresses.get(0).getLatitude();
+                countryLongitude = countryAddresses.get(0).getLongitude();
+
+            }
+            else
+            {
+                receivedCountry = CountryDataSource.DEFAULT_COUNTRY_NAME;
+            }
+        }
+
+
+
+        catch (IOException e) {
+            receivedCountry = CountryDataSource.DEFAULT_COUNTRY_NAME;
+        }
+        LatLng myCountryLocation = new LatLng(countryLatitude,countryLongitude);
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(myCountryLocation,17.2f);
         mMap.moveCamera(cameraUpdate);
+
         MarkerOptions markerOptions = new MarkerOptions();
-        markerOptions.position(sydney);
-        markerOptions.title("Welcome to sydney");
-        markerOptions.snippet("Fantastic");
+        markerOptions.position(myCountryLocation);
+        markerOptions.title(countryMessage);
+        markerOptions.snippet(CountryDataSource.DEFAULT_MESSAGE);
         mMap.addMarker(markerOptions);
 
+
         CircleOptions circleOptions = new CircleOptions();
-        circleOptions.center(sydney);
-        circleOptions.radius(300);
-        circleOptions.strokeWidth(20.0f);
-        circleOptions.strokeColor(Color.CYAN);
+        circleOptions.center(myCountryLocation);
+        circleOptions.radius(400);
+        circleOptions.strokeWidth(14.5f);
+        circleOptions.strokeColor(Color.BLUE);
         mMap.addCircle(circleOptions);
+
     }
 }
